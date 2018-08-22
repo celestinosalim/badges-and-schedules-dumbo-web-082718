4b349ca798e012a1c0e53422ad3820f840e0797a
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-22 15:46:20 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 
 def assign_rooms(speakers)
   list = []
-  room = 1
+  # room = 1
   speakers.each_with_index do |name, index|
     room_assignments = "Hello, #{{name}}! You'll be assigned to room #{index}"
 #   speakers.each do |name|
